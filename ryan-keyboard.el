f2fc327278ed3091d0a7f1823fa578c919346f85
sparkclient <sparkclient@fenghaixiong-pc.(none)>
2015-05-22 18:12:26 +0800

    change shift to alter

--- ryan-keyboard.el
+++ ryan-keyboard.el
@@ -13,10 +13,10 @@
 (global-set-key "\C-z" 'ignore)
 (global-set-key "\C-x\C-z" 'ignore)
 
-(global-set-key [S-left] 'windmove-left)          ; move to left window
-(global-set-key [S-right] 'windmove-right)        ; move to right window
-(global-set-key [S-up] 'windmove-up)              ; move to upper window
-(global-set-key [S-down] 'windmove-down)          ; move to lower window
+(global-set-key [M-left] 'windmove-left)          ; move to left window
+(global-set-key [M-right] 'windmove-right)        ; move to right window
+(global-set-key [M-up] 'windmove-up)              ; move to upper window
+(global-set-key [M-down] 'windmove-down)          ; move to lower window
 
 ;The generic apropos (of any symbol) is MUCH more useful than apropos-command
 (global-set-key "\C-ha" 'apropos)
